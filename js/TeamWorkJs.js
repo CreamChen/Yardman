1025f7b00e29e20a3996cadeeff7c27fa7cadb87
Jachin <874545946@qq.com>
2018-05-07 15:53:28 +0800

    Jachin

--- js/TeamWorkJs.js
+++
@@ -1,9 +0,0 @@
-// TeamWorkJS
-$(function(){
-	// 标签栏切换
-	$('.category li').eq(sessionStorage.getItem('TeamWorkNum')).addClass('category_on');
-	$('.category').on('click','li',function(){
-		$(this).addClass('category_on').siblings().removeClass('category_on');
-		sessionStorage.setItem('TeamWorkNum',$(this).index());
-	})
-})
